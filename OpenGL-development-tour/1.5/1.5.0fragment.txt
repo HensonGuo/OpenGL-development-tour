b0dacfe2ecad699d08ac4dcd37e20cd55f429172
zhxmdefj <zhxmdefj@outlook.com>
2019-11-06 15:45:05 +0800

    1.5变换

---
+++ OpenGL-development-tour/1.5/1.5.0fragment.txt
@@ -0,0 +1,13 @@
+// fragment shader
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
+void main()
+{
+    FragColor = texture(ourTexture, TexCoord);//texture(纹理采样器，对应的纹理坐标)
+}
---
+++ OpenGL-development-tour/1.5/shader.h
@@ -0,0 +1,30 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>; // 包含glad来获取所有的必须OpenGL头文件
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	// 生成一个ID
+	unsigned int ID;
+	// 构造函数
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	// 使用/激活程序
+	void use() { glUseProgram(ID); }
+	// uniform工具函数
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+
+private:
+	// 检查编译或链接是否出错
+	void checkCompileErrors(unsigned int shader, std::string type);
+};
+
+#endif
